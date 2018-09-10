6f70bb83807d619336398fc01620c7201e9e7680
Leszek Trzemecki <leszek.trzemecki@gmali.com>
2018-09-10 18:51:35 +0200

    Add missing install_requires in setup

--- setup.py
+++ setup.py
@@ -1,33 +1,27 @@
-try:
-    from ez_setup import use_setuptools
+from setuptools import setup
 
-    use_setuptools()
-except ImportError:
-    pass
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
 
 __version_info__ = (5, 0, 0)
 __version__ = '.'.join([str(v) for v in __version_info__])
 
-setup(name="Unum",
-      version=__version__,
-      description="Units in Python",
-      author="Chris MacLeod, Pierre Denis, Leszek Trzemecki",
-      author_email="leszek.trzemecki@gmail.com",
-      url="https://github.com/trzemecki/Unum",
-      license="LGPL",
-      setup_requires=[''],
-      py_modules=['ez_setup'],
-      test_suite="tests",
-      packages=('unum',
-                'unum.units',
-                'unum.units.custom',
-                'unum.units.others',
-                'unum.units.si',
-                'tests',
-                )
-      )
+setup(
+    name="Unum",
+    version=__version__,
+    description="Units in Python",
+    author="Chris MacLeod, Pierre Denis, Leszek Trzemecki",
+    author_email="leszek.trzemecki@gmail.com",
+    url="https://github.com/trzemecki/Unum",
+    license="LGPL",
+    install_requires=[
+        'six'
+    ],
+    test_suite="tests",
+    packages=(
+        'unum',
+        'unum.units',
+        'unum.units.custom',
+        'unum.units.others',
+        'unum.units.si',
+        'tests',
+    )
+)
